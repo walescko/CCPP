826a65bd1b5eb70836cdeb490507aeed8a372782
walescko <walescko@gmail.com>
2024-05-18 08:33:42 -0300

    c - eq II complete full file

--- FACP002/P002Ex014.c
+++ FACP002/P002Ex014.c
@@ -5,7 +5,7 @@
 #include <math.h>
 int main (void){
 
-    double a, b, c, delta;//, x1, x2;
+    double a, b, c, delta, x1, x2;
 
     printf("Resolução do Equação do II Grau \n\n");
     printf("Termo quadrático (a): ");
@@ -18,4 +18,18 @@ int main (void){
 
     delta = - pow(b,2) - 4*a*c;
     printf("%f", delta);
+
+    if (delta < 0) {
+        printf("Não existe raízes reais!");
+    } if (delta == 0){
+        x1 = (-b + delta)/ (2*a);
+        printf("Existe uma raiz real: x = %f", x1);
+    } else {
+        x1 = (-b + delta)/ (2*a);
+        x2 = (-b - delta)/ (2*a);
+        printf("Existe duas raízes reais distintas.\n");
+        printf("x1 = %f.\n", x1);
+        printf("x2 = %f.\n", x2);
+    }
+    return 0;
 }
