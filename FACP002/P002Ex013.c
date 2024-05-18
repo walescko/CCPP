0d27846616f1e95c32282c29649a821b7de4daae
walescko <walescko@gmail.com>
2024-05-18 07:02:28 -0300

    c - uniq file

--- FACP002/P002Ex013.c
+++ FACP002/P002Ex013.c
@@ -1,3 +1,20 @@
 //
 // Created by walescko on 5/17/24.
 //
+#include <stdio.h>
+int x;
+void dataRead(void){
+    printf("Entre com um valor inteiro: ");
+    scanf("%d", &x);
+}
+
+void dataPrint(void){
+    printf("O dobro do número %d é %d!", x, (2*x));
+}
+
+int main (void) {
+    dataRead();
+    dataPrint();
+    printf("\nFim do programa!\n");
+    return 0;
+}
---
+++ FACP002/P002Ex014.c
@@ -0,0 +1,19 @@
+//
+// Created by walescko on 5/17/24.
+//
+#include <stdio.h>
+#include <math.h>
+int main (void){
+
+    double a, b, c, delta, x1, x2;
+
+    printf("Resolução do Equação do II Grau \n\n");
+    printf("Termo quadrático (a): ");
+    scanf("%lf", &a);
+    printf("Termo linear (b): ");
+    scanf("%lf", &b);
+    printf("Termo independente (c): ");
+    scanf("%lf", &c);
+    printf("\n\nEquação: %f x² + %f x + %f = 0", a, b, c);
+
+}
