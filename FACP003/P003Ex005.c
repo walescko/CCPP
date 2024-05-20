b09e1aa8855a138515c83a02cf32fa2095c61936
walescko <walescko@gmail.com>
2024-05-20 05:10:35 -0300

    c - long long int

---
+++ FACP003/P003Ex005.c
@@ -0,0 +1,11 @@
+//
+// Created by walescko on 5/20/24.
+//
+#include <stdio.h>
+#include <limits.h>
+
+int main(void){
+    long long int a = LLONG_MAX;
+    printf("a = %lld\n", a);
+    return 0;
+}
