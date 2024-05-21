c6139a27d19d4d06c3160381d2d3ee790c0a8ba5
walescko <walescko@gmail.com>
2024-05-21 04:49:36 -0300

    c - float

---
+++ FACP003/P003Ex030.c
@@ -0,0 +1,12 @@
+//
+// Created by walescko on 5/21/24.
+//
+#include <stdio.h>
+
+int main (void){
+    int a = 16777217, b = 16777219;
+    float c = a;
+    float d = b;
+    printf("%f %f\n", c, d);
+    return 0;
+}
