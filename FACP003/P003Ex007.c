d66851ac5b789f90d1406851b1d1e24831459715
walescko <walescko@gmail.com>
2024-05-21 04:43:44 -0300

    c - int

--- FACP003/P003Ex007.c
+++ FACP003/P003Ex007.c
@@ -1,3 +1,10 @@
 //
 // Created by walescko on 5/20/24.
 //
+#include <stdio.h>
+#include <float.h>
+int main (void) {
+    long double a = LDBL_MAX;
+    printf("a = %Lg\n", a);
+    return 0;
+}
---
+++ FACP003/P003Ex028.c
@@ -0,0 +1,13 @@
+//
+// Created by walescko on 5/21/24.
+//
+#include <stdio.h>
+
+int main (void){
+    int a = 7000;
+    short int b;
+    b = a;
+    printf("%d -> %hd\n", a, b);
+
+    return 0;
+}
