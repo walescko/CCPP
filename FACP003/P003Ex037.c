3de0d6292bea923520e68a3b4f40ed14dfc09b16
walescko <walescko@gmail.com>
2024-05-22 05:00:02 -0300

    c - compatibillity

---
+++ FACP003/P003Ex037.c
@@ -0,0 +1,16 @@
+//
+// Created by walescko on 5/22/24.
+//
+#include <stdio.h>
+int main(void){
+    short a = 2;
+    short int b;
+    signed short int c;
+    b = a;
+    c = a;
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+
+    return 0;
+}
