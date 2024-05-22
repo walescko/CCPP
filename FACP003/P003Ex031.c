00cdaad454d7628c990cbaeee8906d92331799a3
walescko <walescko@gmail.com>
2024-05-21 21:39:30 -0300

    float -> int

---
+++ FACP003/P003Ex031.c
@@ -0,0 +1,11 @@
+//
+// Created by walescko on 5/21/24.
+//
+#include <stdio.h>
+int main (void){
+    double a = 32600.234, b = 40000;
+    short int c = a;
+    short int d = b;
+    printf("%hd %hd\n", c, d);
+    return 0;
+}
