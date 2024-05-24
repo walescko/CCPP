5e2f4bc6684ccb61aefc3abe362c667e5c6452ef
walescko <walescko@gmail.com>
2024-05-24 17:02:33 -0300

    c - alternative representation

---
+++ FACP004/P004Ex010.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+    char c = '\141';
+    
+    printf("%c %hhd\n", c , c);
+
+    return 0;
+    
+}
---
+++ FACP004/P004Ex011.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+
+    char c = 'x61';
+
+    printf("%c %hhd\n", c, c);
+
+    return 0;
+    
+}
