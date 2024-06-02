867e9496667b9806f0dc7dfe14ef2be30b748c72
walescko <walescko@gmail.com>
2024-06-02 04:41:59 -0300

    c - new

---
+++ FACP004/P004Ex025.c
@@ -0,0 +1,19 @@
+//
+// Created by walescko on 5/24/24.
+//
+#include <stdio.h>
+int main(void){
+    int res;
+    int num;
+    scanf("%d", &num);
+    res = num > 4;
+    if (res >4) {
+        printf("num > 4\n");
+
+    }
+    return 0;
+
+
+
+
+}
