2780057fa03fbb4c3afec37e0dd398d0af545eae
walescko <walescko@gmail.com>
2024-06-13 05:07:16 -0300

    c - operators 2

---
+++ FACP006/P006Ex002.c
@@ -0,0 +1,10 @@
+#include <stddio.h>
+int main(void){
+    int a, b, c, d;
+    a = 123 - 2;
+    b = 23 * 3;
+    c = a / b;
+    d = a % d;
+    printf("%d %d %d %d\n", a, b, c, d);
+    return 0;
+}
