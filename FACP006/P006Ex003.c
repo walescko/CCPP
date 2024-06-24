fa926b25775c32a24782b0acdc411f47d9da86bb
walescko <walescko@gmail.com>
2024-06-24 06:39:38 -0300

    c - type variables

--- FACP006/P006Ex003.c
+++ FACP006/P006Ex003.c
@@ -1,3 +1,17 @@
 //
 // Created by walescko on 6/24/24.
 //
+#include <stdio.h>
+int main(void){
+    int a, b; double c, d; float e, f;
+    a = 3 * 2;
+    b = 9 / a;
+    c = 3 * 2.0;
+    d = 9.0 / a;
+    e = 2 + 3 *(a / 2.0F);
+    f = 2 * e;
+    printf("%d %d %f\n", a , b ,c);
+    printf("%f %f %f\n", d, e , f);
+    return 0;
+
+}
