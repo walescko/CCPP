0bd57f6a38a208efafe7682b2c4625cd9cdef4e9
walescko <walescko@gmail.com>
2024-07-07 04:47:19 -0300

    if else

---
+++ FACP007/P007Ex003.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void){
+    int a;
+    
+    printf("Digite um número inteiro positivo: ");
+    scanf("%d", &a);
+    
+    printf("\nO número digitado é: %d\n", a);
+    if (a > 30) {
+        printf("%d é maior que 30.\n", a);    
+    } else {
+        printf("%d é menor ou igual a 30.\n", a);
+    }
+    return 0;
+
+}
